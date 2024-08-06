0ea1c6671eb16333ab12b5cdc96d9b44a624b84a
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-08-06 11:29:43 -0600

    Update using Express

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname, './public/about.html')));
+
+app.get('/contact-me', (req, res) => res.sendFile(path.join(__dirname, './public/contact-me.html')));
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/404.html')));
+
+app.listen(port);
+console.log(`Running on port: ${port}`);
